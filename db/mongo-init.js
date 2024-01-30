f4b184fb9a4c88b98ca04b8c6e611410bac9ffe6
Adam <ad4sk01234@gmail.com>
2024-01-30 20:32:18 +0100

    Add inventory service db configuration

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -1,11 +1,18 @@
 const orderDbName = "orders";
+const inventoryDbName = "inventory";
 db.getSiblingDB(orderDbName);
+db.getSiblingDB(inventoryDbName);
 
 const admin_db = db.getSiblingDB("admin");
 admin_db.createUser({
   user: process.env["ORDERS_USER_USERNAME"],
   pwd: process.env["ORDERS_USER_PASSWORD"],
   roles: [{ role: "readWrite", db: orderDbName }],
+});
+admin_db.createUser({
+  user: process.env["INVENTORY_USER_USERNAME"],
+  pwd: process.env["INVENTORY_USER_PASSWORD"],
+  roles: [{ role: "readWrite", db: inventoryDbName }],
 });
 
 
---
+++ inventory-service/src/main/java/com/example/inventory/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.example.inventory.controller;
+
+import com.example.inventory.service.ProductService;
+import com.example.models.*;
+import io.micronaut.core.async.annotation.SingleResult;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.*;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Single;
+import jakarta.validation.Valid;
+
+@Controller("/products")
+public class ProductController {
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @Get
+  Flowable<Product> list() {
+    return productService.findAll();
+  }
+
+  @Post
+  @Status(HttpStatus.CREATED)
+  @SingleResult
+  Single<Product> save(@Valid Product product) {
+    return productService.save(product);
+  }
+
+  @Put
+  @SingleResult
+  Single<Product> update(@Valid Product product) {
+    return productService.save(product);
+  }
+
+  @Get("/{id}")
+  @SingleResult
+  Maybe<Product> find(@PathVariable String id) {
+    return productService.findById(id);
+  }
+}
