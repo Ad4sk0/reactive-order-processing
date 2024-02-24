be471f701c20e563ff17b9953bb9be8c70fcb466
Adam <ad4sk01234@gmail.com>
2024-02-24 12:22:36 +0100

    Add embedded Artemis MQ to delivery service

--- delivery-service/src/main/java/com/example/delivery/Application.java
+++ delivery-service/src/main/java/com/example/delivery/Application.java
@@ -1,10 +1,13 @@
 package com.example.delivery;
 
 import io.micronaut.runtime.Micronaut;
+import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
 
 public class Application {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    EmbeddedActiveMQ embedded = new EmbeddedActiveMQ();
+    embedded.start();
     Micronaut.run(Application.class, args);
   }
 }
