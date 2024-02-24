f9aa459e06918ba6d2453a19c30a12b4934af98f
Adam <ad4sk01234@gmail.com>
2024-02-24 13:17:14 +0100

    Add delivery statuses and descriptions

--- models/src/main/java/com/example/models/DeliveryStatus.java
+++ models/src/main/java/com/example/models/DeliveryStatus.java
@@ -1,8 +1,18 @@
 package com.example.models;
 
 public enum DeliveryStatus {
+  /** Until delivery is planned */
   INITIALIZING,
+
+  /** After delivery job is enqueued for processing and planned for delivery */
+  PLANNED,
+
+  /** After delivery job has started */
   IN_DELIVERY,
+
+  /** After delivery job is completed */
   DELIVERED,
-  END,
+
+  /** After delivery job is cancelled */
+  CANCELLED
 }
